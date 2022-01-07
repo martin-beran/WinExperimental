5f484ac4ab634c0d89f26975fc2d61898ed77f14
Martin Beran <martin@mber.cz>
2022-01-07 23:56:06 +0100

    WiP: PCapFile

--- PCapFile/PCapFile.cpp
+++ PCapFile/PCapFile.cpp
@@ -1,6 +1,48 @@
 // PCapFile.cpp : Defines the functions for the static library.
 //
 
+#include "PCapFile.h"
+
 #include "pch.h"
 #include "framework.h"
 
+#include <iomanip>
+#include <sstream>
+
+/*** PCapFileError *****************************************************************/
+
+std::string PCapFileError::createMessage(const char* msg, DWORD error)
+{
+	std::ostringstream os;
+	os << msg << ": 0x" << std::hex << error;
+	return os.str();
+}
+
+/*** PCapFile **********************************************************************/
+
+PCapFile::~PCapFile()
+{
+	try {
+		close();
+	} catch (const PCapFileError&) {
+	}
+}
+
+void PCapFile::create()
+{
+	fh = CreateFileW(fileName.c_str(), GENERIC_WRITE, FILE_SHARE_READ, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL,
+		nullptr);
+	if (fh == INVALID_HANDLE_VALUE)
+		throw PCapFileError("Cannot open file", GetLastError());
+	PCapHdr hdr;
+}
+
+void PCapFile::close()
+{
+	if (fh != INVALID_HANDLE_VALUE && !CloseHandle(fh))
+		throw PCapFileError("Cannot close file", GetLastError());
+}
+
+void PCapFile::writePacket(const std::string_view data, std::optional <std::chrono::system_clock::time_point>)
+{
+}
--- PCapFile/PCapFile.h
+++ PCapFile/PCapFile.h
@@ -1,21 +1,52 @@
 #pragma once
 
+#include <windows.h>
+
 #include <chrono>
+#include <stdexcept>
 #include <optional>
 #include <string_view>
 
+class PCapFileError: std::runtime_error {
+public:
+	PCapFileError(const char* msg, DWORD error): runtime_error(createMessage(msg, error)) {}
+private:
+	static std::string createMessage(const char* msg, DWORD error);
+};
+
 class PCapFile {
 public:
+	PCapFile() = default;
 	PCapFile(const std::wstring_view fileName): fileName(fileName) {}
 	PCapFile(const PCapFile&) = delete;
-	PCapFile(PCapFile&&) = delete;
+	PCapFile(PCapFile&& o) {
+		std::swap(fileName, o.fileName);
+		std::swap(fh, o.fh);
+	}
 	PCapFile& operator=(const PCapFile&) = delete;
-	PCapFile&& operator=(PCapFile&&) = delete;
+	PCapFile& operator=(PCapFile&& o) {
+		if (&o != this) {
+			fileName = o.fileName;
+			o.fileName.clear();
+			fh = o.fh;
+			o.fh = INVALID_HANDLE_VALUE;
+		}
+		return *this;
+	}
 	~PCapFile();
 	void create();
 	void close();
-	void writePacket(const std::string_view data, std::optional<std::chrono::system_clock::time_point);
+	void writePacket(const std::string_view data, std::optional<std::chrono::system_clock::time_point>);
 private:
+	struct PCapHdr {
+		uint32_t magicNumber;
+		uint16_t versionMajor;
+		uint16_t versionMinor;
+		int32_t thiszone;
+		uint32_t sigfigs;
+		uint32_t snaplen;
+		uint32_t network;
+	};
 	std::wstring fileName;
-	HANDLE fh = INVALID_HANDLE_FILE;
+	HANDLE fh = INVALID_HANDLE_VALUE;
 };
