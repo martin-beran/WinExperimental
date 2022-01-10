77ecd7494f166c7214c297dbd1b02dd451bec244
Martin Beran <martin@mber.cz>
2022-01-11 00:30:56 +0100

    Header pointer adjustment not needed on injection
    by NdisRetreatNetBufferListDataStart()
    - bug: invalid checksums of injected packets

--- PacketDriver/PacketDriver.h
+++ PacketDriver/PacketDriver.h
@@ -62,7 +62,6 @@ struct PacketInfo {
 	IF_INDEX interfaceIdx;
 	IF_INDEX subinterfaceIdx;
 	size_t size;
-	UINT32 ipHeaderSize;
 };
 
 #define IOCTL_PACKETDRIVER_GET_STATS CTL_CODE(PACKETDRIVER_DEVICE_TYPE, 0x800, METHOD_BUFFERED, FILE_WRITE_DATA)
